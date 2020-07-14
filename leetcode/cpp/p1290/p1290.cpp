219be5adf3ae9f82c869dbe15d0022696db9fad0
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-14 13:11:29 -0500

    Added problem 1290  - Add solution to problem 1290  - Add test cases for problem 1290  - Add problem 1290 to README

---
+++ leetcode/cpp/p1290/p1290.cpp
@@ -0,0 +1,46 @@
+//Source: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+//Date: 14.07.2020
+//Solution by: David Luna
+//Runtime: 0ms
+//Memory usage: 8.4 MB
+
+#include <iostream>
+#include "../lib/linkedListUtils/linkedListUtils.h"
+
+using namespace std;
+
+// Leetcode solution starts
+
+class Solution {
+    public:
+        int getDecimalValue(ListNode* head) {
+            int ans = 0;
+            while(head != nullptr){
+                ans <<= 1;
+                ans += head->val;
+                head = head->next;
+            }
+            return ans;
+        }
+};
+
+// Leetcode solution ends
+
+void makeTest(){
+    int numElements, correctAns;
+    cin >> numElements;
+    ListNode* linkedList = makeLinkedList(numElements);
+    cin >> correctAns;
+    cout << (correctAns == Solution().getDecimalValue(linkedList) ? "pass\n" : "fail\n");
+    deleteLinkedList(linkedList);
+}
+
+int main(){
+    int numTests;
+    //Introduce the number of tests to make.
+    cin >> numTests;
+    for(int i = 0; i < numTests; i++){
+        makeTest();
+    }
+    return 0;
+}
