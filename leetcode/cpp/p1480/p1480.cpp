d33bc9305d3c3b78967dca2a7c6efe869528d8e0
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-17 13:21:05 -0500

    Added problem 1480  - Add solution to problem 1480  - Add test cases for problem 1480  - Add problem 1480 to README

---
+++ leetcode/cpp/p1480/p1480.cpp
@@ -0,0 +1,54 @@
+//Source: https://leetcode.com/problems/running-sum-of-1d-array/
+//Date: 17.07.2020
+//Solution by: David Luna
+//Runtime: 4ms
+//Memory usage: 8.6 MB
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Leetcode solution starts
+
+class Solution {
+    public:
+        vector<int> runningSum(vector<int>& nums) {
+            int size = nums.size();
+            for(int i = 1; i < size; i++){
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+};
+
+// Leetcode solution ends
+
+template <typename T>
+void makeVectorT(vector<T>& vect, int numElements){
+    for(int i = 0; i < numElements; i++){
+        T temp;
+        cin >> temp;
+        vect.push_back(temp);
+    }
+}
+
+void makeTest(){
+    int numElements;
+    vector<int> nums, correctAns;
+    cin >> numElements;
+    makeVectorT(nums, numElements);
+    makeVectorT(correctAns, numElements);
+    nums = Solution().runningSum(nums);
+    cout << (nums == correctAns ? "pass\n" : "fail\n");
+}
+
+int main(){
+    int numTests;
+    //Introduce the number of tests to make.
+    cin >> numTests;
+    for(int i = 0; i < numTests; i++){
+        makeTest();
+    }
+    return 0;
+}
