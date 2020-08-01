5094a6a371c0ff063a7299487ebaaee941578b9d
davidlunadeleon <davidlunadeleon@gmail.com>
2020-08-01 10:41:35 -0500

    Add problem of august 2020 challenge week 1 day 1
    - Add solution to problem Detect Capital
    - Add test cases for problem Detect Capital
    - Add problem Detect Capital to README

---
+++ leetcode/leetcoding_challenges/README.md
@@ -0,0 +1,17 @@
+# Leetcoding Challenges
+
+This section contains my solutions to some of the challenges posted on leetcode.
+
+## About the contents of this directory
+
+Each challenge has a dedicated directory where solutions can be found with a `.cpp` file and a `testCases` file, just like the main directory of problems
+
+## How to use the test cases
+
+Please refed to the main [README](../../README.md)
+
+## Problems solved
+
+| Challenge   | Problem ans source                                                                                                                          | Language                                            |
+| ----------- | ------------------------------------------------------------------------------------------------------------------------------------------- | --------------------------------------------------- |
+| August 2020 | August 1st [Detect Capital](https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3409/) | [C++](august_2020/week1/august1/detect_capital.cpp) |
---
+++ leetcode/leetcoding_challenges/august_2020/week1/august1/detect_capital.cpp
@@ -0,0 +1,51 @@
+// Source:
+// https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3409/
+// Date: 01.08.2020
+// Solution by: David Luna
+// Runtime: 0ms
+// Memory usage: 6 MB
+
+#include <iostream>
+
+using namespace std;
+
+// Leetcode solution starts
+
+class Solution {
+  public:
+	bool detectCapitalUse(string word) {
+		bool allCaps = true, allLowerCase = true;
+		int size = word.size();
+		for (int i = 1; i < size; i++) {
+			allCaps = allCaps && word[i] < 'a';
+			allLowerCase = allLowerCase && word[i] >= 'a';
+			if (!(allLowerCase || allCaps)) {
+				return false;
+			}
+		}
+		if (allCaps && word[0] >= 'a' && size > 1) {
+			return false;
+		}
+		return true;
+	}
+};
+
+// Leetcode solution ends
+
+void makeTest() {
+	string word;
+	bool ans, correctAns;
+	cin >> word >> correctAns;
+	ans = Solution().detectCapitalUse(word);
+	cout << (ans == correctAns ? "pass\n" : "fail\n");
+}
+
+int main() {
+	int numTests;
+	// Introduce the number of tests to make.
+	cin >> numTests;
+	for (int i = 0; i < numTests; i++) {
+		makeTest();
+	}
+	return 0;
+}
