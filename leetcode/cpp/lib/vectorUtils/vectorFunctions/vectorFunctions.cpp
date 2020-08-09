daf7bf1e2ba02ec65d754b4c628f1f5c5c527894
davidlunadeleon <davidlunadeleon@gmail.com>
2020-08-09 15:04:16 -0500

    Move template functions  in vectorFunctions to .h

--- leetcode/cpp/lib/vectorUtils/vectorFunctions/vectorFunctions.cpp
+++ leetcode/cpp/lib/vectorUtils/vectorFunctions/vectorFunctions.cpp
@@ -1,31 +1,3 @@
 // Made by David Luna
 
-#include <iostream>
-#include <vector>
-
 #include "./vectorFunctions.h"
-
-template <typename T> void makeVectorT(std::vector<T> &vect, int size) {
-	vect.clear();
-	if (size <= 0) {
-		std::cin >> size;
-	}
-	for (int i = 0; i < size; i++) {
-		T temp;
-		std::cin >> temp;
-		vect.push_back(temp);
-	}
-}
-
-template <typename T>
-void makeMatrixT(std::vector<std::vector<T>> &matrix, int r, int c) {
-	matrix.clear();
-	if (r <= 0 || c <= 0) {
-		std::cin >> r >> c;
-	}
-	for (int i = 0; i < r; i++) {
-		std::vector<T> temp;
-		makeVectorT(temp, c);
-		matrix.push_back(temp);
-	}
-}
--- leetcode/cpp/lib/vectorUtils/vectorFunctions/vectorFunctions.h
+++ leetcode/cpp/lib/vectorUtils/vectorFunctions/vectorFunctions.h
@@ -5,10 +5,32 @@
 #ifndef _VECTOR_FUNCTIONS
 #define _VECTOR_FUNCTIONS
 
+#include <iostream>
 #include <vector>
 
-template <typename T> void makeVectorT(std::vector<T> &vect, int size = 0);
+template <typename T> void makeVectorT(std::vector<T> &vect, int size) {
+	vect.clear();
+	if (size <= 0) {
+		std::cin >> size;
+	}
+	for (int i = 0; i < size; i++) {
+		T temp;
+		std::cin >> temp;
+		vect.push_back(temp);
+	}
+}
+
 template <typename T>
-void makeMatrixT(std::vector<std::vector<T>> &matrix, int r = 0, int c = 0);
+void makeMatrixT(std::vector<std::vector<T>> &matrix, int r, int c) {
+	matrix.clear();
+	if (r <= 0 || c <= 0) {
+		std::cin >> r >> c;
+	}
+	for (int i = 0; i < r; i++) {
+		std::vector<T> temp;
+		makeVectorT(temp, c);
+		matrix.push_back(temp);
+	}
+}
 
 #endif
