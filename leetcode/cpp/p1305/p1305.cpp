ec7eed03a88f0a341e1f43487e3c3c61995eee4e
davidlunadeleon <davidlunadeleon@gmail.com>
2020-09-05 09:47:50 -0500

    [C++] Update problem 1305
    - Use vectorUtils
    - Remove 'using namespace std'

--- leetcode/cpp/p1305/p1305.cpp
+++ leetcode/cpp/p1305/p1305.cpp
@@ -1,33 +1,35 @@
 // Source:
 // https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
-// Date: 20.07.2020
+// Date: 05.09.2020
 // Solution by: David Luna
-// Runtime: 348ms
-// Memory usage: 84.9 MB
+// Runtime: 288ms
+// Memory usage: 85.1 MB
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
 #include "../lib/treeUtils/treeUtils.h"
-
-using namespace std;
+#include "../lib/vectorUtils/vectorUtils.h"
 
 // Leetcode solution starts
 
 class Solution {
   public:
-	vector<int> getAllElements(TreeNode *root1, TreeNode *root2) {
-		vector<int> list;
+	std::vector<int> getAllElements(TreeNode *root1, TreeNode *root2) {
+		std::vector<int> list;
+		int middle;
+
 		makeInorderVector(root1, list);
-		int middle = list.size();
+		middle = list.size();
 		makeInorderVector(root2, list);
 		inplace_merge(list.begin(), list.begin() + middle, list.end());
+
 		return list;
 	}
 
   private:
-	void makeInorderVector(TreeNode *node, vector<int> &vect) {
+	void makeInorderVector(TreeNode *node, std::vector<int> &vect) {
 		if (node != nullptr) {
 			makeInorderVector(node->left, vect);
 			vect.push_back(node->val);
@@ -38,33 +40,27 @@ class Solution {
 
 // Leetcode solution ends
 
-template <typename T> void makeVectorT(vector<T> &vect) {
-	int numElements;
-	cin >> numElements;
-	for (int i = 0; i < numElements; i++) {
-		T temp;
-		cin >> temp;
-		vect.push_back(temp);
-	}
-}
-
 void makeTest() {
-	vector<string> tree1, tree2;
+	std::vector<std::string> tree1, tree2;
 	TreeNode *root1, *root2;
-	vector<int> correctAns, ans;
+	std::vector<int> correctAns, ans;
+
 	makeVectorT(tree1);
 	makeVectorT(tree2);
 	makeVectorT(correctAns);
 	root1 = makeTree(tree1);
 	root2 = makeTree(tree2);
+
 	ans = Solution().getAllElements(root1, root2);
-	cout << (ans == correctAns ? "pass\n" : "fail\n");
+	std::cout << (ans == correctAns ? "pass\n" : "fail\n");
+
+	deleteTree(root1);
+	deleteTree(root2);
 }
 
 int main() {
 	int numTests;
-	// Introduce the number of tests to make.
-	cin >> numTests;
+	std::cin >> numTests;
 	for (int i = 0; i < numTests; i++) {
 		makeTest();
 	}
